ec2b9e439298598c40cf3cc06d57998ca53a9c2a
Mike Rix Wolfe <2893679+MikeRixWolfe@users.noreply.github.com>
2026-02-07 12:36:43 -0600

    Update keymap.c

--- keyboards/lily58/keymaps/mikefightsbears/keymap.c
+++ keyboards/lily58/keymaps/mikefightsbears/keymap.c
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_GRV,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_DEL, \
-  KC_F1,   _______, _______, KC_UP,   _______, _______,                   _______, _______, KC_PGUP, _______, KC_PSCR, KC_F12, \
-  _______, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   _______, KC_HOME, KC_PGDN, KC_END,  _______, KC_INS, \
+  KC_F1,   _______, _______, KC_UP,   _______, _______,                   _______, _______, _______, _______, KC_PSCR, KC_F12, \
+  _______, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, KC_INS, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY, _______, \
                              _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU \
 ),
